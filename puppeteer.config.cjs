0e1b3f5b0aa15adee7139d5b6ff69c64fd4cb798
utkarsh <55540613+whoisseth@users.noreply.github.com>
2023-07-25 23:14:01 +0530

    Create puppeteer.config.cjs

---
+++ puppeteer.config.cjs
@@ -0,0 +1,7 @@
+/** @format */
+
+const { join } = require("path");
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = { cacheDirectory: join(__dirname, ".cache", "puppeteer") };
